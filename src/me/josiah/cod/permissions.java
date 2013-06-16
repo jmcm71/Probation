211055b3377553b41f8b4d9ef90c08f058c41da6
Josiah <josiahmartin@live.com>
2013-06-16 16:35:07 -0500

    Revert "My source for probation"
    This reverts commit 6df5d3b17be7157bed7cdbbc9c22348d363db71c.

--- src/me/josiah/cod/permissions.java
+++
@@ -1,8 +0,0 @@
-package me.josiah.cod;
-
-import org.bukkit.permissions.Permission;
-
-public class permissions {
-	public Permission canuse = new Permission("onduty.use");
-	public Permission canusegui = new Permission("onduty.gui");
-}
