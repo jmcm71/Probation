6df5d3b17be7157bed7cdbbc9c22348d363db71c
Josiah <josiahmartin@live.com>
2013-06-16 16:28:19 -0500

    My source for probation

---
+++ src/me/josiah/cod/permissions.java
@@ -0,0 +1,8 @@
+package me.josiah.cod;
+
+import org.bukkit.permissions.Permission;
+
+public class permissions {
+	public Permission canuse = new Permission("onduty.use");
+	public Permission canusegui = new Permission("onduty.gui");
+}
